0ceea149d444fe7415bb7d4be1ee826db19a6e55
LimitlessGreen <limitlessgreen@outlook.de>
2026-08-15 01:16:01 +0200

    fix: ensure all build fixes are staged and committed

--- src/services/webdis/WebdisFileSystem.ts
+++ src/services/webdis/WebdisFileSystem.ts
@@ -10,6 +10,8 @@ export const DEFAULT_WEBDIS_CONFIG: WebdisConfig = {
 };
 
 class RawWebdisFs implements RawFs {
+  constructor(_config: WebdisConfig) {}
+
   async walk(_fullPath: string): Promise<Entity[]> {
     throw new Error("Not implemented");
   }
